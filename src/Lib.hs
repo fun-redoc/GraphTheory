1e5f9f69583f0849b5a699adcacd0f3df9e1b8d7
fun-redoc <fun.redoc@gmail.com>
2020-02-07 16:54:59 +0100

    adding shortes path unweighted

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
 {-# Language GADTs #-}
 {-# Language MultiParamTypeClasses #-}
 {-# Language FlexibleInstances #-}
@@ -9,6 +12,8 @@ module Lib
     ( someFunc
     ) where
 
+import BasicPrelude
+import CorePrelude
 import qualified Data.HashMap.Lazy as M
 import qualified Data.HashSet as S
 import Data.Hashable (Hashable)
@@ -21,17 +26,14 @@ class Graph g a b where
   add_edge::a->a->b->g a b->g a b
   add_edge_undir::a->a->b->g a b->g a b
   get_indegrees::g a b->M.HashMap a Int
+  all_nodes::g a b->[a]
 
 data AdjGraph a b = AdjGraph {getMap::M.HashMap a (M.HashMap a b)} deriving Show
 mkAdjGraph = AdjGraph (M.empty)
 instance (Eq a, Hashable a, Ord b)=> Graph AdjGraph a b where
   num_vertices (AdjGraph g) = M.size g
   adjacent_vertices (AdjGraph g) vertex = S.toList $  M.keysSet (g M.! vertex)
-  add_vertex v (AdjGraph g) = AdjGraph (M.alter (\tryAdj -> Just $ maybe M.empty id tryAdj
---                                                    case tryAdj of
---                                                        Nothing->Just M.empty 
---                                                        Just adj->Just adj
-                                                ) v g)
+  add_vertex v (AdjGraph g) = AdjGraph (M.alter (\tryAdj -> Just $ maybe M.empty id tryAdj) v g)
   connect v1 v2 w (AdjGraph g) = AdjGraph (M.adjust (\adj -> M.insert v2 w adj) v1 g)
   add_edge v1 v2 w = connect v1 v2 w . add_vertex v1 . add_vertex v2
   add_edge_undir v1 v2 w = connect v2 v1 w .connect v1 v2 w . add_vertex v1 . add_vertex v2
@@ -39,6 +41,8 @@ instance (Eq a, Hashable a, Ord b)=> Graph AdjGraph a b where
     M.foldr (\adj acc ->
                 foldr (\v acc'-> M.adjust (+1) v acc') acc $ M.keys adj
             ) (M.map (const 0) g) g
+  all_nodes (AdjGraph g) = M.keys g
+
 
 
 breadth_first::(Graph g a b, Eq a, Hashable a)=>g a b->a->[a]
@@ -69,12 +73,45 @@ topological_sort g = sortt queue indegrees [] where
     let sorted' = sorted++[v]
         adj = adjacent_vertices g v
         (indegrees', queue') = foldr (\v2 (ind, qu) ->
-                                        let ind' = M.adjust (\i->i-1) v2 ind
-                                            qu' = if ind' M.! v2 == 0 then qu++[v2] else qu
-                                         in (ind', qu')
-                                     ) (indegrees, qtail) adj
+                                      let ind' = M.adjust (\i->i-1) v2 ind
+                                          qu' = if ind' M.! v2 == 0 then qu++[v2] else qu
+                                       in (ind', qu')
+                                   ) (indegrees, qtail) adj
      in sortt queue' indegrees' sorted'
 
+distance_matrix g start = iterate initial_dm start (M.keys $ M.delete start initial_dm) where
+  initial_dm = foldr (\v acc-> 
+                        if (v == start)
+                        then (M.insert v (0, Just v) acc)
+                        else (M.insert v (-1, Nothing) acc)
+                    ) M.empty $ all_nodes g
+  iterate dm s (v:vs) = if null vs then dm' else iterate dm' v vs where
+    start_dm = dm M.! s
+    start_dist = fst start_dm
+    prev = snd start_dm
+    dm' = foldr (\v' ->M.adjust (\d -> case d of
+                                         (_, Nothing) -> (start_dist+1, Just s)
+                                         (last_dist, last_vertx) -> if last_dist <= start_dist
+                                                                    then (last_dist, last_vertx)
+                                                                    else (start_dist+1, Just s)
+                                       --if Nothing == snd d 
+                                       --then (start_dist+1, Just s)
+                                       --else d
+                                ) v'
+                ) dm (adjacent_vertices g s)
+ 
+backtrack::(Show a, Eq a, Hashable a, Applicative t, Foldable t, Monoid (t a))=>a->a->M.HashMap a (Int, Maybe a)->t a
+backtrack start dest dist_mat = iterate dest (pure dest) where
+    iterate dest' res = if dest' == start then res else iterate prev_vert res' where
+        prev_dist = dist_mat M.! dest'
+        prev_vert = maybe (error ((show dest')++"is not member if dist matrix")) id $ snd prev_dist
+        res' = (pure prev_vert) <> res
+
+  
+
+shortest_path_unweighted::(Graph g a d, Show a, Eq a, Hashable a, Applicative t, Foldable t, Monoid (t a))=>g a d->a->a->t a
+shortest_path_unweighted g start dest =
+    backtrack start dest $ distance_matrix g start
 
 someFunc :: IO ()
 someFunc = do
@@ -100,6 +137,24 @@ someFunc = do
                 . add_edge 3 4 1
                 . add_edge 6 8 1
                 $ mkAdjGraph
-    putStrLn $ show $ breadth_first ugraph 2
-    putStrLn $ show $ get_indegrees dgraph
-    putStrLn $ show $ topological_sort dgraph
+    putStrLn $ tshow $ breadth_first ugraph 2
+    putStrLn $ tshow $ get_indegrees dgraph
+    putStrLn $ tshow $ topological_sort dgraph
+    putStrLn $ tshow $ distance_matrix dgraph 0
+    let dgraph1 = add_edge(0::Int) 1 1
+                . add_edge_undir 1 2 1
+                . add_edge_undir 1 3 1
+                . add_edge_undir 2 3 1
+                . add_edge_undir 1 4 1
+                . add_edge_undir 3 5 1
+                . add_edge_undir 5 4 1
+                . add_edge_undir 3 6 1
+                . add_edge_undir 6 7 1
+                . add_edge_undir 0 7 1
+                $ mkAdjGraph
+    putStrLn "---------------------------------"
+    putStrLn $ tshow $ dgraph1
+    putStrLn $ tshow $ distance_matrix dgraph1 0
+    putStrLn $ tshow $ ((shortest_path_unweighted dgraph1 0 5)::[Int])
+    putStrLn $ tshow $ ((shortest_path_unweighted dgraph1 0 6)::[Int])
+    putStrLn $ tshow $ ((shortest_path_unweighted dgraph1 7 4)::[Int])
